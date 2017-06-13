349e3566bcb1a5771fd8388c435326c9dbca0856
xiwi92 <616340233@qq.com>
2017-06-13 17:51:21 +0800

    update

--- src/main/PicMysqlJson.go
+++ src/main/PicMysqlJson.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/emirpasic/gods/sets/hashset"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -49,7 +48,7 @@ func (p Pic) add() (ID int, err error) {
 // https://github.com/emirpasic/gods
 func main() {
 	var err error
-	db, err = sql.Open("mysql", "root:root123xw@tcp(127.0.0.1:3306)/goginapi?parseTime=true")
+	db, err = sql.Open("mysql", "xiwi:root123@tcp(123.207.242.90:3306)/go_api?parseTime=true")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -59,10 +58,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	///Users/xiwi/Documents/design/pic/emotions/json.json
+
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/animals.json")
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/arch.json")
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/backgrounds.json")
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/car.json")
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/industry.json")
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/places.json")
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/plant.json")
+	file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/data/people.json")
+	////
+	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/best/json.json")
 	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/new/json.json")
 	// file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/emotions/json.json")
-	file, e := ioutil.ReadFile("/Users/xiwi/Documents/design/pic/best/json.json")
 	if e != nil {
 		fmt.Printf("File error: %v\n", e)
 		os.Exit(1)
@@ -77,21 +85,21 @@ func main() {
 	for _, pic := range jsonPics {
 		ID, _ := pic.add()
 		fmt.Println(ID)
-		// logger.Println(pic)
+		logger.Println(pic)
 	}
 
-	set := hashset.New()   // empty
-	set.Add(1)             // 1
-	set.Add(2, 2, 3, 4, 5) // 3, 1, 2, 4, 5 (random order, duplicates ignored)
-	logger.Println(set)
-	set.Remove(4)      // 5, 3, 2, 1 (random order)
-	set.Remove(2, 3)   // 1, 5 (random order)
-	set.Contains(1)    // true
-	set.Contains(1, 5) // true
-	set.Contains(1, 6) // false
-	_ = set.Values()   // []int{5,1} (random order)
-	set.Clear()        // empty
-	set.Empty()        // true
-	set.Size()         // 0
+	// set := hashset.New()   // empty
+	// set.Add(1)             // 1
+	// set.Add(2, 2, 3, 4, 5) // 3, 1, 2, 4, 5 (random order, duplicates ignored)
+	// logger.Println(set)
+	// set.Remove(4)      // 5, 3, 2, 1 (random order)
+	// set.Remove(2, 3)   // 1, 5 (random order)
+	// set.Contains(1)    // true
+	// set.Contains(1, 5) // true
+	// set.Contains(1, 6) // false
+	// _ = set.Values()   // []int{5,1} (random order)
+	// set.Clear()        // empty
+	// set.Empty()        // true
+	// set.Size()         // 0
 
 }
